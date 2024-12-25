cc293222f49be0c0254d32c22e601ce56dbe7d59
nischhal-nub <nischalbh51@gmail.com>
2024-12-25 07:26:19 +0545

    fix: digi-menu

--- client/turn2dine/src/pages/no-auth-pages/digital-menu/page.tsx
+++ client/turn2dine/src/pages/no-auth-pages/digital-menu/page.tsx
@@ -1,16 +1,16 @@
 import { PhoneCall, Soup } from "lucide-react";
 import { Button } from "../../../components/ui/button";
 import DishCard from "./_components/dish-card";
-import { toast } from "sonner";
-import { Toaster } from "../../../components/ui/sonner";
+import { toast, Toaster } from "sonner";
 import {
   Sheet,
   SheetContent,
-  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "../../../components/ui/sheet"
+import OrderSummary from "./_components/order-summary";
+import { Fragment } from "react/jsx-runtime";
 
 export default function DigitalMenu() {
   return (
@@ -24,36 +24,51 @@ export default function DigitalMenu() {
         <p className="text-lg text-stone-950 font-medium pt-2">Motomania Cafe & Workshop</p>
       </div>
 
-      {/* button section */}
-      <div className="sticky top-4 flex items-center justify-center gap-4 mt-4 bg-background">
-        <Button variant="secondary" className="text-white" onClick={() => toast("Waiter called.")}><PhoneCall />Call Waiter</Button>
-        <Sheet>
-          <SheetTrigger><Button variant="outline"><Soup />My Orders</Button></SheetTrigger>
-          <SheetContent>
-            <SheetHeader className="mb-4">
-              <SheetTitle className="text-left">My orders</SheetTitle>
-            </SheetHeader>
-            <DishCard/>
-            <SheetFooter className="mt-4">
-              <Button className="w-full">Place order</Button>
-            </SheetFooter>
-          </SheetContent>
-        </Sheet>
-      </div>
+      <div className="sticky top-0 bg-background">
+        {/* button section */}
+        <div className="py-4 flex items-center justify-center gap-4 mt-4 ">
+          <Button variant="secondary" className="text-white" onClick={() => toast("Waiter called.",{
+            position: "top-right",
+            duration: 3000,
+            description: "Please wait. A waiter will be at your service soon."
+          })}><PhoneCall />Call Waiter</Button>
+          <Sheet>
+            <SheetTrigger><Button variant="outline"><Soup />My Orders</Button></SheetTrigger>
+            <SheetContent className="overflow-y-scroll">
+              <SheetHeader className="mb-4">
+                <SheetTitle className="text-left">My orders</SheetTitle>
+              </SheetHeader>
+              <div className="space-y-4">
+                {Array.from({ length: 10 }).map((_, i) => (
+                  <Fragment key={i}>
+                    <OrderSummary />
+                  </Fragment>
+                ))}
+              </div>
+              <div className="fixed bottom-0 py-4 bg-background w-[62%]">
+                <p className="pb-2 mb-2 font-semibold border-b-2 border-border">Total Price: Rs 2000</p>
 
-      {/* dish type section */}
-      <div className="sticky top-20 mt-4 flex items-center justify-evenly overflow-y-scroll gap-2 bg-background">
-        {
-          Array.from({ length: 10 }).map((_, i) => (
-            <div key={i} className="px-2 py-[2px] bg-black text-white rounded-md">
-              <p className="text-nowrap text-base">Dish type {i + 1}</p>
-            </div>
-          ))
-        }
+                <Button className="w-full">Place order</Button>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+
+        {/* dish type section */}
+        <div className=" mt-4 flex items-center justify-evenly overflow-y-scroll gap-2 ">
+          {
+            Array.from({ length: 10 }).map((_, i) => (
+              <div key={i} className="px-2 py-[2px] bg-black text-white rounded-md">
+                <p className="text-nowrap text-base">Dish type {i + 1}</p>
+              </div>
+            ))
+          }
+        </div>
+        <p className="text-xl font-semibold mt-4">Dish Type 1</p>
+
       </div>
       {/* dishes section */}
       <div className="mt-4">
-        <p className="sticky top-28 text-xl font-semibold bg-background">Dish Type 1</p>
         <div className="grid grid-cols-2 mt-4 gap-2 pb-4">
           <DishCard />
           <DishCard />
