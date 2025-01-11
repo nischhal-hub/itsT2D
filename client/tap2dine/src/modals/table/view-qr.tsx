e7a6688d2bd5819fb39944df4e20f9291dbb7a6b
nischhal-nub <nischalbh51@gmail.com>
2025-01-11 07:46:59 +0545

    feat: print qr

--- client/tap2dine/src/modals/table/view-qr.tsx
+++ client/tap2dine/src/modals/table/view-qr.tsx
@@ -3,11 +3,17 @@ import { ModalType } from "../../types/modal.types";
 import { DIGITAL_MENU_LOGO } from "../../constants/images";
 import { Button } from "../../components/ui/button";
 import { PrinterCheck } from "lucide-react";
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 
 export default function ViewQr({ data }: ModalType<"VIEW_QR">) {
+  const componentRef = useRef<HTMLDivElement>(null);
+  const handlePrint = useReactToPrint({
+    contentRef: componentRef,
+  });
   return (
     <div>
-      <div className="border rounded-md py-4">
+      <div className="border rounded-md py-4" ref={componentRef}>
         <div className="flex w-full items-center justify-center p-2">
           <img src={DIGITAL_MENU_LOGO} alt="logo" width={70} />
         </div>
@@ -18,7 +24,7 @@ export default function ViewQr({ data }: ModalType<"VIEW_QR">) {
         <p className="text-sm font-medium text-center">Today's Menu</p>
       </div>
       <div className="mt-4">
-        <Button variant={"outline"}>
+        <Button variant={"outline"} onClick={() => handlePrint()}>
           <PrinterCheck />
           Print QR
         </Button>
