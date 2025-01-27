170f9ff049438979f86957c9502923f852b2355c
nischhal-nub <nischalbh51@gmail.com>
2025-01-27 06:44:06 +0545

    add loading state to pages

--- client/tap2dine/src/pages/auth-pages/dashboard/page.tsx
+++ client/tap2dine/src/pages/auth-pages/dashboard/page.tsx
@@ -1,18 +1,33 @@
 import { useFetchDishes } from "../../../api/queries/dish.query";
 import { useFetchOrders } from "../../../api/queries/orders.query";
 import PageHeader from "../../../components/reusables/page-header";
+import TableFetchLoader from "../../../components/reusables/table-fetch-loader";
 import { DailyStats } from "./_components/daily-stats";
 import { MenuManagement } from "./_components/menu-management";
 import { OrdersOverview } from "./_components/orders-overview";
 
 export default function Dashboard() {
-  const { data: dishData } = useFetchDishes();
-  const { data: orders } = useFetchOrders();
+  const { data: dishData , isLoading:dishFetching } = useFetchDishes();
+  const { data: orders, isLoading:ordersFetching } = useFetchOrders();
+  const isLoading = dishFetching || ordersFetching
   return (
     <div>
       <PageHeader title="Dashboard" />
       <div>
-        <div className="flex gap-4">
+        {
+          isLoading ? (
+            <div className="space-y-4">
+              <div className="flex space-x-4">
+                  <div className="h-48 w-full bg-gray-200 rounded animate-pulse" />
+                  <div className="h-48 w-full bg-gray-200 rounded animate-pulse" />
+                </div>
+                <div>
+                <div className="h-72 w-full bg-gray-200 rounded animate-pulse" />
+                </div>
+            </div>
+          ) : (
+            <>
+            <div className="flex gap-4">
           {
             orders &&
             <>
@@ -27,6 +42,10 @@ export default function Dashboard() {
             <MenuManagement data={dishData} />
           }
         </div>
+        </>
+          )
+        }
+        
       </div>
     </div>
   )
